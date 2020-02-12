bbc183f39a014daa2f49c5b05541eb247a0f2a61
KristineVs <chienviernes26@gmail.com>
2020-02-12 13:04:19 +0000

    The all seeing eye

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -14,15 +14,18 @@ public class HelloProcessing extends PApplet
 
 
 	public void draw() {
-		background(0, 0, 255);
-		stroke(255, 0, 0);
+		background(255, 0, 0);
+		//stroke(255, 0, 0);
 		fill(255, 255, 0);
-		line(10, 10, 100, 100);		// x1, y1, x2, y2
-		point(50, 50); // x, y
-		ellipse(100, 200, 50, 150); // cx, cy, w, h
-		fill(0, 255, 0);
-		rect(50, 10, 100, 200); // tlx, tly, w, h
-		triangle(10, 10, 100, 90, 200, 200); // vertices	
+		//line(10, 10, 100, 100);		// x1, y1, x2, y2
+		//point(50, 50); // x, y
+		ellipse(250,300, 400, 400); // cx, cy, w, h
+		fill(0, 255, 255);
+		triangle(20, 450, 250, 50, 480, 450); // vertices
+		fill(180);	
+		ellipse(250, 250, 220, 100);
+		fill(0);
+		ellipse(250, 250, 80, 80);
 	}	
 	
 
